d3fd105ca7b02fc5c755e90deaeeb12d70a46330
Ana Caballero <ana.caballero@gmx.de>
2020-12-04 11:21:59 +0100

    refactor into MVC pattern, add error messages when no pokemon or info

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,27 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-let jsonData = require('../pokemonData.js');
+const { getAllPokemon, getOnePokemon, getOnePokemonInfo } = require("../controllers/pokemon");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send(jsonData);
-});
-
-
-router.get('/:id', function(req, res, next) {
-  const pokemonID = req.params.id;
-  const singlePokemon = jsonData.find(item => parseInt(item.id) === parseInt(pokemonID));
-  
-  res.send(singlePokemon);
-});
-router.get('/:id/:info', function(req, res, next) {
-  const pokemonID = req.params.id;
-  const pokemonInfo = req.params.info;
-  const singlePokemon = jsonData.find(item => parseInt(item.id) === parseInt(pokemonID));
-  
-  res.send(singlePokemon[pokemonInfo]);
-});
+router.get('/', getAllPokemon);
+router.get('/:id', getOnePokemon);
+router.get('/:id/:info', getOnePokemonInfo);
 
 module.exports = router;
 
